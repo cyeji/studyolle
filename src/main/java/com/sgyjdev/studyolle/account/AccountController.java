b0059637d9ee88268235bf75998b7456f9aef80d
cyeji <choyeji1591@gmail.com>
2023-08-03 21:57:03 +0900

    feat : 회원가입 시 데이터 검증로직 추가

--- src/main/java/com/sgyjdev/studyolle/account/AccountController.java
+++ src/main/java/com/sgyjdev/studyolle/account/AccountController.java
@@ -1,15 +1,38 @@
 package com.sgyjdev.studyolle.account;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
 
-    @GetMapping("/sign-up")
-    public String signUpForm(Model model){
-        return "account/sign-up";
+    private final SignUpFormValidator signUpFormValidator;
 
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model) {
+        model.addAttribute("signUpForm", new SignUpForm());
+        return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/sgyjdev/studyolle/account/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.sgyjdev.studyolle.account;
+
+import com.sgyjdev.studyolle.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
