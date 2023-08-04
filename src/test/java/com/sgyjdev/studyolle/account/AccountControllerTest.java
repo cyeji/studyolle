952a5e83beb3e11055595225aa05441163ae6ce1
cyeji <choyeji1591@gmail.com>
2023-08-04 19:55:22 +0900

    test : 인증메일확인 테스트 추가

--- src/test/java/com/sgyjdev/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/sgyjdev/studyolle/account/AccountControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 class AccountControllerTest {
@@ -33,12 +35,32 @@ class AccountControllerTest {
     @Autowired
     private JavaMailSender javaMailSender;
 
+
+    @Test
+    @DisplayName("인증 메일 확인 - 입력값 오류")
+    void checkEmailToken_with_wrong_input() throws Exception {
+        mockMvc.perform(get("/check-email-token").param("token", "sgge").param("email", "email@email.com")).andExpect(status().isOk())
+            .andExpect(model().attributeExists("error")).andExpect(view().name("account/checked-email"));
+    }
+
+
+    @Test
+    @DisplayName("인증 메일 확인 - 입력값 정상")
+    void checkEmailToken() throws Exception {
+        Account account = Account.builder().email("test@email.com").password("12345678").nickname("yeji").build();
+        Account newAccount = accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
+
+        mockMvc.perform(get("/check-email-token").param("token", newAccount.getEmailCheckToken()).param("email", newAccount.getEmail()))
+            .andExpect(status().isOk()).andExpect(model().attributeDoesNotExist("error")).andExpect(model().attributeExists("nickname"))
+            .andExpect(model().attributeExists("numberOfUser")).andExpect(view().name("account/checked-email"));
+    }
+
     @Test
     @DisplayName("회원 가입 화면 보이는지 테스트")
     void signUpForm() throws Exception {
         mockMvc.perform(get("/sign-up")).andExpect(status().isOk()).andExpect(view().name("account/sign-up")).andExpect(model().attributeExists("signUpForm"));
     }
-
 
     @Test
     @DisplayName("회원가입처리 - 입력값 오류")
