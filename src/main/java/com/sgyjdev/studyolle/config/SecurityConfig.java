664fe52e6c32bf52c2eed240d9bb893066505be7
cyeji <choyeji1591@gmail.com>
2023-08-05 21:21:44 +0900

    feat : security ignore 설정 추가

--- src/main/java/com/sgyjdev/studyolle/config/SecurityConfig.java
+++ src/main/java/com/sgyjdev/studyolle/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.sgyjdev.studyolle.config;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,5 +32,10 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(antMatcher("/node_modules/**")).requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
 }
